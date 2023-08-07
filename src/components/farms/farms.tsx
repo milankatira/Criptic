7d3fd954001bbbcee0d06cee64e8304f03ea0c77
milankatira26@gmail.com <milankatira26@gmail.com>
2023-08-07 14:38:23 +0530

    fix code

--- src/components/farms/farms.tsx
+++ src/components/farms/farms.tsx
@@ -56,76 +56,84 @@ export default function Farms() {
           <Loader variant="blink" />
         </div>
       )}
-      <div className="rounded-tl-lg rounded-tr-lg bg-white px-4 pt-6 dark:bg-light-dark md:px-8 md:pt-8">
-        <div className="flex flex-col items-center justify-between border-b border-dashed border-gray-200 pb-5 dark:border-gray-700 md:flex-row">
-          <h2 className="mb-3 shrink-0 text-lg font-medium uppercase text-black dark:text-white sm:text-xl md:mb-0 md:text-2xl">
-            Top news
-          </h2>
-        </div>
-      </div>
-      <div className="flex flex-row flex-wrap">
-        {data &&
-          data?.length > 0 &&
-          data.map(
-            (item: {
-              url: string | undefined;
-              datePublished: string | Date;
-              image: { thumbnail: { contentUrl: any } };
-              name: string;
-              description:
-                | string
-                | number
-                | boolean
-                | ReactElement<any, string | JSXElementConstructor<any>>
-                | ReactFragment
-                | ReactPortal
-                | null
-                | undefined;
-              provider: {
-                name: string;
-                image: { thumbnail: { contentUrl: string | undefined } };
-              }[];
-            }) => (
-              <div
-                className="p-4 transition-all duration-300 md:w-1/3"
-                key={item.name}
-              >
-                <div className="h-full overflow-hidden  rounded-lg border-2 border-gray-200 border-opacity-60 p-6">
-                  <div className="flex flex-row gap-2">
-                    <h2 className="title-font mb-1 text-xl font-bold">
-                      <a href={item.url} target="_blank" rel="noreferrer">
-                        {item.name}
-                      </a>
-                    </h2>
-                    <img
-                      className="h-28 w-28 rounded-lg object-cover object-center"
-                      src={
-                        item?.image?.thumbnail?.contentUrl ||
-                        'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News'
-                      }
-                      alt="blog"
-                    />
-                  </div>
+      {!isFetching && (
+        <>
+          <div className="rounded-tl-lg rounded-tr-lg bg-white px-4 pt-6 dark:bg-light-dark md:px-8 md:pt-8">
+            <div className="flex flex-col items-center justify-between border-b border-dashed border-gray-200 pb-5 dark:border-gray-700 md:flex-row">
+              <h2 className="mb-3 shrink-0 text-lg font-medium uppercase text-black dark:text-white sm:text-xl md:mb-0 md:text-2xl">
+                Top news
+              </h2>
+            </div>
+          </div>
+          <div className="flex flex-row flex-wrap">
+            {data &&
+              data?.length > 0 &&
+              data.map(
+                (item: {
+                  url: string | undefined;
+                  datePublished: string | Date;
+                  image: { thumbnail: { contentUrl: any } };
+                  name: string;
+                  description:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | ReactFragment
+                    | ReactPortal
+                    | null
+                    | undefined;
+                  provider: {
+                    name: string;
+                    image: { thumbnail: { contentUrl: string | undefined } };
+                  }[];
+                }) => (
+                  <div
+                    className="p-4 transition-all duration-300 md:w-1/3"
+                    key={item.name}
+                  >
+                    <div className="h-full overflow-hidden  rounded-lg border-2 border-gray-200 border-opacity-60 p-6">
+                      <div className="flex flex-row gap-2">
+                        <h2 className="title-font mb-1 text-xl font-bold">
+                          <a href={item.url} target="_blank" rel="noreferrer">
+                            {item.name}
+                          </a>
+                        </h2>
+                        <img
+                          className="h-28 w-28 rounded-lg object-cover object-center"
+                          src={
+                            item?.image?.thumbnail?.contentUrl ||
+                            'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News'
+                          }
+                          alt="blog"
+                        />
+                      </div>
 
-                  <div className="pt-6">
-                    <p className="mb-3 leading-relaxed">{item.description}</p>
-                    <div className="flex flex-wrap items-center ">
-                      <img
-                        className="h-12 w-12 object-cover object-center"
-                        src={item?.provider[0]?.image?.thumbnail?.contentUrl}
-                        alt="blog"
-                      />
-                      <p className="ml-2">{item?.provider[0]?.name}</p>
-                      <p className="ml-auto">
-                        {getTimeAgo(item.datePublished)}
-                      </p>
+                      <div className="pt-6">
+                        <p className="mb-3 leading-relaxed">
+                          {item.description}
+                        </p>
+                        <div className="flex flex-wrap items-center ">
+                          <img
+                            className="h-12 w-12 object-cover object-center"
+                            src={
+                              item?.provider[0]?.image?.thumbnail?.contentUrl
+                            }
+                            alt="blog"
+                          />
+                          <p className="ml-2">{item?.provider[0]?.name}</p>
+                          <p className="ml-auto">
+                            {getTimeAgo(item.datePublished)}
+                          </p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            )
-          )}
-      </div>
+                )
+              )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
